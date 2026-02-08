81445260d4608ac9280373eb70b6639366977593
baghbaninfratech <ashuraza456@gmail.com>
2026-02-08 15:16:47 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,14 +7,24 @@ const FILES_TO_CACHE = [
   "./manifest.json"
 ];
 
-self.addEventListener("install", (event)=>{
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event)=>{
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(res=> res || fetch(event.request))
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
