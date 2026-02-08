60e403ef55564e8b9d525a0d483b5b284097b0f8
baghbaninfratech <ashuraza456@gmail.com>
2026-02-08 15:32:08 +0530

    Refactor service worker by removing activate event
    Removed the 'activate' event listener and its associated logic.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
 const CACHE_NAME = "sultan-mart-v1";
+
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
@@ -11,16 +12,6 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    )
-  );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
